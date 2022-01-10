9d9107cd91310a577b9457ef9c82e9e70e094f49
Michael Dougherty <mikedoc.p.doc.71@gmail.com>
2022-01-10 15:48:02 -0600

    improved validation for atx_header

--- R/atx_header.R
+++ R/atx_header.R
@@ -8,6 +8,10 @@
 #' @return String of ATX-style markdown header.
 #'
 atx_header <- function (level) {
+  if(!is.numeric(level)) {stop ("level must be numeric")}
   if(level%%1 != 0) { stop("level must be an integer") }
+  if(level == 0) {stop("level cannot be zero")}
+  if(level > 5) {stop("level cannot reasonably be greater than five")}
+
   paste(rep.int("#", level), collapse = "")
 }
