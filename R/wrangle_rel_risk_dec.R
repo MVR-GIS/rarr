1d2bd71f80c828905d18c3554242a3e0dd9be78d
Barrie Chileen <barrie.chileen@gmail.com>
2022-11-16 09:42:27 -0600

    finished migrating wrangle_rel_risk_dec function

--- R/wrangle_rel_risk_dec.R
+++ R/wrangle_rel_risk_dec.R
@@ -1,48 +1,48 @@
 #' @title Wrangle Rel Risk Decisions
 #'
-#' @description Formats an input `rel_risk_dec` data frame and prepares
+#' @description Wrangles an input `rel_risk_dec` data frame and prepares
 #'   it for use by the the reporting functions in this package.
 #'
 #' @export
-#' @param db_rel_dec_dec   data frame; A standard `rel_dec_dec` data frame.
+#' @param db_rel_risk_dec   data frame; A standard `rel_risk_dec` data frame.
 #'
 #' @return A formatted data frame suitable for use by the report functions.
 #'
 #' @examples
 #' # Get test data
-#' rel_risk_dec <- rarr::db_rel_dec_dec
+#' db_rel_risk_dec <- rarr::db_rel_risk_dec
 #'
 #' # Format rel_risk_dec
-#' rel_dec_dec <- format_rel_dec_dec(db_rel_risk_dec)
+#' rel_risk_dec <- wrangle_rel_risk_dec(db_rel_risk_dec)
 #'
 #' @importFrom dplyr filter relocate
 #' @importFrom rlang .data
 #'
+#'
 wrangle_rel_risk_dec<- function(db_rel_risk_dec, active = TRUE) {
+  # Check parameters
+  if(!is.data.frame(db_rel_risk_dec)) {
+    stop("db_rel_risk_dec must be a data frame")}
+
+# Remove test records
  risk<- rarr::remove_test_records(db_rel_risk_dec, "RISK_NO")
  risk<- rarr::remove_test_records(risk, "DECISION_NO")
- risk <- risk %>%
-   filter(RISK_ACTIVE == "Yes" & DECISION_ACTIVE =="Yes")
-}
-# Remove test records
-#rel_risk_dec <- remove_test_records(rel_risk_dec_oracle, "RISK_NO")
-#rel_risk_dec <- remove_test_records(rel_risk_dec, "DECISION_NO")
 
 # Filter for "Active" records
-#rel_risk_dec <- rel_risk_dec %>%
-  #filter(RISK_ACTIVE == "Yes" & DECISION_ACTIVE == "Yes")
+ risk <- risk %>%
+   filter(RISK_ACTIVE == "Yes" & DECISION_ACTIVE =="Yes")
 
 # Cleanup id fields for sorting
-rel_risk_dec <- format_id(rel_risk_dec, "RISK_NO")
-rel_risk_dec <- format_id(rel_risk_dec, "DECISION_NO")
+ risk <- rarr::format_id(risk, "RISK_NO")
+ risk <- rarr::format_id(risk, "DECISION_NO")
 
 # Create hyperlink
-rel_risk_dec <- id_link(rel_risk_dec, "risk_no")
-rel_risk_dec <- id_link(rel_risk_dec, "decision_no")
+ risk <- rarr::id_link(risk, "risk_no")
+ risk <- rarr::id_link(risk, "decision_no")
 
 # Reorder fields
-rel_risk_dec <- rel_risk_dec %>%
-  relocate(DECISION_ACTIVE, .after = decision_no_link)
+ rel_risk_dec <- risk %>%
+   relocate(DECISION_ACTIVE, .after = decision_no_link)
 
-# Cleanup
-#rm(rel_risk_dec_oracle)
+ return(rel_risk_dec)
+}
